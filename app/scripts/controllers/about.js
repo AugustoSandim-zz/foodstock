23cc5d125e9f975c2588349ed9ef766d41764893
Augusto Sandim <augusto@jera.com.br>
2015-12-16 17:26:57 -0300

    Commit initial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodstockApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the foodstockApp
+ */
+angular.module('foodstockApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
